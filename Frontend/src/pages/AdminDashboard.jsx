14ef767dfc2e415a0d3ca8c568c0eccb298ec1c5
Godwin <godwinaugustine118@gmail.com>
2026-06-09 03:11:51 +0100

    improve admin dashboard

--- Frontend/src/pages/AdminDashboard.jsx
+++ Frontend/src/pages/AdminDashboard.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { logoutAdmin } from "../utils/auth";
+import { Printer, Download, RefreshCcw, LogOut  } from "lucide-react";
 
 export default function AdminDashboard() {
   const [records, setRecords] = useState([]);
@@ -21,29 +22,57 @@ export default function AdminDashboard() {
     setTimeout(() => fetchRecords(), 2000);
   }, []);
 
+  const exportCSV = () => {
+    const headers = ["S/n", "Name", "ID", "Time"];
+  
+    const rows = records.map((r, i) =>
+      [i+1, r.name, r.id, new Date(r.time).toLocaleString()]
+    );
+  
+    let csvContent =
+      "data:text/csv;charset=utf-8," +
+      [headers, ...rows].map(e => e.join(",")).join("\n");
+  
+    const encodedUri = encodeURI(csvContent);
+    const link = document.createElement("a");
+  
+    link.setAttribute("href", encodedUri);
+    link.setAttribute("download", "attendance.csv");
+    document.body.appendChild(link);
+    link.click();
+  };
+
   return (
     <div className="adminPage">
 
       <div className="tableCard">
         <h2>Attendance Records</h2>
 
-        <button className="refreshBtn" onClick={fetchRecords}>
-          Refresh
-        </button>
-
-        <button
-          className="logoutBtn"
-          onClick={() => {
-            logoutAdmin();
-            navigate("/admin-login");
-          }}
-        >
-          Logout
-        </button>
-
+        <div className="buttons">
+          <button className="refreshBtn" onClick={fetchRecords}>
+            <RefreshCcw size={18} />
+          </button>
+          <button className="refreshBtn" onClick={() => window.print()}>
+            <Printer size={18} />
+          </button> 
+          <button className="refreshBtn" onClick={exportCSV}>
+            <Download size={18} />
+          </button>
+          <button
+            className="logoutBtn"
+            onClick={() => {
+              logoutAdmin();
+              navigate("/admin-login");
+            }}
+          >
+            <LogOut size={18} />
+          </button>
+        </div>
+        <div className="tableWrapper">
         <table>
           <thead>
             <tr>
+              <th>S/n</th>
               <th>Name</th>
               <th>ID</th>
               <th>Time</th>
@@ -53,14 +82,15 @@ export default function AdminDashboard() {
           <tbody>
             {records.map((r, i) => (
               <tr key={i}>
+                <td>{i + 1}</td> 
                 <td>{r.name}</td>
                 <td>{r.id}</td>
                 <td>{new Date(r.time).toLocaleString()}</td>
               </tr>
             ))}
           </tbody>
-        </table>
-
+          </table>
+        </div>
       </div>
     </div>
   );
